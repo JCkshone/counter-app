f1ea2751272a5e680c78e167ff6711abcbd8181a
Juan Camilo Navarro <jcinfonet.kshone@gmail.com>
2020-12-01 23:26:02 -0500

    Initial Commit

---
+++ counterApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  counterApp
+//
+//  Created by Juan Camilo on 1/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
